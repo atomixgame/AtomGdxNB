ef559240ea4b4857acd263a2072e357eb8bf3025
AtomGdx Developer <dev@atomgdx.com>
2026-08-16 20:55:49 -0400

    feat(options): implement LibGDX category in NetBeans Options Dialog with AtomGdxOptionsPanelController

---
+++ modules/atomgdx-ui-theme/src/main/java/com/atomgdx/theme/options/AtomGdxOptionsPanelController.java
@@ -0,0 +1,94 @@
+package com.atomgdx.theme.options;
+
+import com.atomgdx.core.settings.AtomGdxSettings;
+import com.atomgdx.theme.AtomGdxPreferencesPanel;
+import org.netbeans.spi.options.OptionsPanelController;
+import org.openide.util.HelpCtx;
+import org.openide.util.Lookup;
+
+import javax.swing.*;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+/**
+ * Standard NetBeans Options Panel Controller for the LibGDX category in the NetBeans Options Dialog.
+ * Ref: https://netbeans.apache.org/tutorial/main/tutorials/nbm-options/
+ */
+@OptionsPanelController.SubRegistration(
+        location = "LibGDX",
+        displayName = "#AdvancedOption_DisplayName_LibGDX",
+        keywords = "#AdvancedOption_Keywords_LibGDX",
+        keywordsCategory = "LibGDX"
+)
+public class AtomGdxOptionsPanelController extends OptionsPanelController {
+
+    private AtomGdxPreferencesPanel panel;
+    private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+    private boolean changed;
+
+    @Override
+    public void update() {
+        getPanel();
+        changed = false;
+    }
+
+    @Override
+    public void applyChanges() {
+        SwingUtilities.invokeLater(() -> {
+            if (isChanged()) {
+                getPanel().saveSettings();
+                changed = false;
+            }
+        });
+    }
+
+    @Override
+    public void cancel() {
+        // Discard changes
+    }
+
+    @Override
+    public boolean isValid() {
+        return getPanel() != null;
+    }
+
+    @Override
+    public boolean isChanged() {
+        return changed;
+    }
+
+    @Override
+    public HelpCtx getHelpCtx() {
+        return null;
+    }
+
+    @Override
+    public JComponent getComponent(Lookup masterLookup) {
+        return getPanel();
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener l) {
+        pcs.addPropertyChangeListener(l);
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener l) {
+        pcs.removePropertyChangeListener(l);
+    }
+
+    private AtomGdxPreferencesPanel getPanel() {
+        if (panel == null) {
+            panel = new AtomGdxPreferencesPanel(new AtomGdxSettings());
+        }
+        return panel;
+    }
+
+    public void changed() {
+        if (!changed) {
+            changed = true;
+            pcs.firePropertyChange(OptionsPanelController.PROP_CHANGED, false, true);
+        }
+        pcs.firePropertyChange(OptionsPanelController.PROP_VALID, null, null);
+    }
+}
